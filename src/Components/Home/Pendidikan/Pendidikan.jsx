53046a7ce278c8225e2edd76bc9f76dc2dabb4ce
Dimas1402 <anugerahdimas469@gmail.com>
2019-12-26 08:53:50 +0700

    ok

--- src/Components/Home/Pendidikan/Pendidikan.jsx
+++ src/Components/Home/Pendidikan/Pendidikan.jsx
@@ -5,7 +5,9 @@ const Pendidikan = () => {
   return (
     <div className='row'>
       <div className='col-md-12 col-pendidikan text-center'>
+        
         <h1 style={{ color: '#00aa88ff' }}>Fokus Pendidikan</h1>
+        
       </div>
       <div className='col-md-12 mb-5 pb-5'>
         <div className='row'>
@@ -30,12 +32,9 @@ const Pendidikan = () => {
           <div className='col-md-3'>
             <div className='card-box-pen'>
               <div className='face face1'>
-                <div className='content'>
-                  <img
-                    alt=''
-                    src='https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20card-box-pen%20Hover%20Effects/img/design_128.png?raw=true'
-                  />
-                  <h3>Design</h3>
+                <div className='content text-center'>
+                <i className="fa fa-cogs"></i>
+                  <h3>Backend Development</h3>
                 </div>
               </div>
               <div className='face face2'>
@@ -51,12 +50,9 @@ const Pendidikan = () => {
           <div className='col-md-3'>
             <div className='card-box-pen'>
               <div className='face face1'>
-                <div className='content'>
-                  <img
-                    alt=''
-                    src='https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20card-box-pen%20Hover%20Effects/img/design_128.png?raw=true'
-                  />
-                  <h3>Design</h3>
+                <div className='content text-center'>
+                 <i className="fa fa-mobile-alt"></i>
+                  <h3>Mobile Development</h3>
                 </div>
               </div>
               <div className='face face2'>
@@ -72,12 +68,9 @@ const Pendidikan = () => {
           <div className='col-md-3'>
             <div className='card-box-pen'>
               <div className='face face1'>
-                <div className='content'>
-                  <img
-                    alt=''
-                    src='https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20card-box-pen%20Hover%20Effects/img/design_128.png?raw=true'
-                  />
-                  <h3>Design</h3>
+                <div className='content text-center'>
+                <i className="fa fa-gamepad"></i>
+                  <h3>Game Development</h3>
                 </div>
               </div>
               <div className='face face2'>
