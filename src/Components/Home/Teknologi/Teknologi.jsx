215b8a4b6dc4367e6559baab5688c9bb5d62352f
Dimas1402 <anugerahdimas469@gmail.com>
2019-12-29 10:08:06 +0700

    dimas ==> slider auto play

--- src/Components/Home/Teknologi/Teknologi.jsx
+++ src/Components/Home/Teknologi/Teknologi.jsx
@@ -20,6 +20,10 @@ class Teknologi extends React.Component {
         new Swiper('.swiper-container', {
         slidesPerView: 4,
         spaceBetween: 30,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
         pagination: {
             el: '.swiper-pagination',
             clickable: true
